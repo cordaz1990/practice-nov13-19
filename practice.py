f88d7eb5dfdda3d45fd650fa123c47cf479c18ce
Cynthia Ordaz <cordaz@dons.usfca.edu>
2020-01-20 21:22:08 -0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,4 +1,15 @@
-print("hello")
-print("world")
-print("hello")
-print()
+class Atom:
+  """ An atom with a number, symbol, and coordinates."""
+  def __init__(self, num: init, sym: str, x: float, y:float, z:float) -> None:
+      
+      """ Create an Atom with number num, string symbol sym, and float coordinates(x,y,z).
+      """
+      self.number = num
+      self.center = (x,y,z)
+      self.symbol = sym
+      
+  def __str__(self) -> str:
+      """Return a string representation of this Atom in this format:
+        (SYMBOL, X, Y,Z)
+      """
+      return'({0}, {1}, {2}, {3})'.format(self.symbol, self.center[0], self.center[1], self.center[2])
